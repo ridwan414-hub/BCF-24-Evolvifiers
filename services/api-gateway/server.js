a7d143802288274b021e2ee9415f5fd3ad21d2cc
ridwan <asifmahmoud414@gmail.com>
2024-10-24 14:20:12 +0600

    added api-gateway service

---
+++ services/api-gateway/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const authMiddleware = require('./middleware/authMiddleware');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Proxy configuration
+const authServiceProxy = createProxyMiddleware({
+    target: process.env.AUTH_SERVICE_URL,
+    changeOrigin: true
+});
+const trainServiceProxy = createProxyMiddleware({
+    target: process.env.TRAIN_SERVICE_URL,
+    changeOrigin: true
+});
+const bookingServiceProxy = createProxyMiddleware({
+    target: process.env.BOOKING_SERVICE_URL,
+    changeOrigin: true
+});
+const ticketServiceProxy = createProxyMiddleware({
+    target: process.env.TICKET_SERVICE_URL,
+    changeOrigin: true
+});
+
+// Routes configuration
+app.use('/api/auth', authServiceProxy);
+
+
+app.use('/api/trains', authMiddleware, trainServiceProxy);
+app.use('/api/bookings', authMiddleware, bookingServiceProxy);
+app.use('/api/tickets', authMiddleware, ticketServiceProxy);
+
+// Health check
+app.get('/api/health', (req, res) => {
+    res.json({ message: "API Gateway is running!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`API Gateway running on port ${PORT}`);
+});
--- services/auth-service/models/User.js
+++ services/auth-service/models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    bookedSeats: {
+        ref: 'Seat',
+        type: mongoose.Schema.Types.ObjectId
+    },
     contactNumber: {
         type: String
     }
